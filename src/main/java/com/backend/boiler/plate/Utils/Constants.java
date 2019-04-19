807f22ab78d73e0c4ca9faafcd3c003ea196962c
bittokazi <bitto.kazi@gmail.com>
2019-04-19 15:02:41 +0600

    initial-commit

---
+++ src/main/java/com/backend/boiler/plate/Utils/Constants.java
@@ -0,0 +1,7 @@
+package com.backend.boiler.plate.Utils;
+
+public class Constants {
+
+	public static String ROLE_SUPER_ADMIN = "ROLE_SUPER_ADMIN";
+	public static String ROLE_USER = "ROLE_USER";
+}
